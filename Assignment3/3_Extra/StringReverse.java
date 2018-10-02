5529a16111f1fcf2be638220ec12f18c847913b0
slpclmdpt <43046042+slpclmdpt@users.noreply.github.com>
2018-10-01 23:25:12 -0700

    Add files via upload

---
+++ Assignment3/3_Extra/StringReverse.java
@@ -0,0 +1,32 @@
+package Assignment3_Extra;
+
+import java.util.Scanner;
+
+public class StringReverse {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please input a string:");
+		String s = sc.nextLine();
+		System.out.println("After reverse:" + reverse(s));
+	}
+
+	public static String reverse(String s) {
+		// Reverse all
+		s = new StringBuffer(s).reverse().toString();
+		//Recover the order of the inner of every word 
+		String[] split=s.split(" ");
+		String reversedString="";
+		for(String temp:split) {
+			temp=new StringBuffer(temp).reverse().toString();
+				reversedString+=temp;
+				if(!temp.equals(split[split.length-1])){
+					reversedString+=" ";
+				}
+		}
+		// Delete leading and trailing space
+		reversedString = reversedString.trim();
+		//Delete multiple spaces in the middle
+		reversedString=reversedString.replaceAll(" +", " ");
+		return reversedString;
+	}
+}
